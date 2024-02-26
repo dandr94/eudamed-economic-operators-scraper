d1f23f5ae0bc14660489934feccda381dc5bae8f
dandr94 <d.andreev1994@gmail.com>
2024-02-26 17:27:18 +0200

    Various improvements: Added documentation, type hinting...

--- pages/utils/page_helper.py
+++ pages/utils/page_helper.py
@@ -1,47 +1,90 @@
+from typing import Tuple, List, Union
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import expected_conditions as EC
 
-from selenium.common import NoSuchElementException, TimeoutException
+from selenium.common import TimeoutException
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 
 class PageHelper:
-    def __init__(self, driver, wait_time):
+    """
+        A utility class providing helper methods for interacting with web pages.
+    """
+
+    def __init__(self, driver: WebDriver, wait_time: int):
+        """
+            Initializes the PageHelper with a WebDriver instance and wait time.
+
+            Args:
+                - driver: WebDriver instance for browser automation.
+                - wait_time: The maximum time to wait for an element to appear on the page, in seconds.
+        """
         self.driver = driver
         self.wait = WebDriverWait(self.driver, wait_time)
 
-    def get_url(self, url):
+    def get_url(self, url: str) -> None:
+        """
+            Navigates the browser to the specified URL.
+
+            Args:
+                - url: The URL to navigate to.
+        """
         self.driver.get(url)
 
-    def load_manufacturers(self):
-        url = 'https://ec.europa.eu/tools/eudamed/#/screen/search-eo?actorTypeCode=refdata.actor-type.manufacturer' \
+    def load_url(self, role):
+        """
+            Loads a specific URL based on the role of the actor.
+
+            Args:
+                - role: The role of the actor for which the URL is to be loaded.
+        """
+        url = f'https://ec.europa.eu/tools/eudamed/#/screen/search-eo?actorTypeCode=refdata.actor-type.{role}' \
               '&submitted=true'
 
         self.get_url(url)
 
-    def load_importers(self):
-        url = 'https://ec.europa.eu/tools/eudamed/#/screen/search-eo?actorTypeCode=refdata.actor-type.importer' \
-              '&submitted=true'
+    def wait_for_presence(self, location: Tuple[str, str]) -> Union[WebElement, str]:
+        """
+            Waits for the presence of an element identified by the given location on the page.
 
-        self.get_url(url)
+            Args:
+                - location: A tuple representing the location strategy and value (e.g., (By.ID, 'element_id')).
 
-    def wait_for_presence(self, location):
+            Returns:
+                - The located WebElement upon successful presence.
+                - A timeout error message if the element is not found within the specified time.
+        """
         try:
             return self.wait.until(EC.presence_of_element_located(location))
         except TimeoutException as e:
             return f'Timeout for presence Exception error! \nMessage: {e}'
 
-    def scroll_to_element(self, element):
+    def scroll_to_element(self, element: WebElement) -> None:
+        """
+            Scrolls the page to bring the specified element into view.
+
+            Args:
+                - element: The WebElement to which the page should be scrolled.
+        """
         self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
 
-    def accept_cookies(self):
+    def accept_cookies(self) -> None:
+        """
+            Accepts cookies by clicking on the corresponding button.
+        """
         cookies_location = (By.XPATH, "//a[@href='#accept']")
 
         cookies = self.wait_for_presence(cookies_location)
 
         cookies.click()
 
-    def close_cookies_prompt_after_accept(self):
+    def close_cookies_prompt_after_accept(self) -> None:
+        """
+            Closes the cookies prompt after accepting them.
+        """
         prompt_close_button_location = (By.CLASS_NAME, 'wt-ecl-message__close')
 
         prompt_close_button = self.wait_for_presence(prompt_close_button_location)
@@ -49,14 +92,42 @@ class PageHelper:
         prompt_close_button.click()
 
     @staticmethod
-    def find_element(element, location):
+    def find_element(element: WebElement, location: Tuple[str, str]) -> WebElement:
+        """
+            Finds and returns a single WebElement within the specified element's scope.
+
+            Args:
+                - element: The parent WebElement within which to search.
+                - location: A tuple representing the location strategy and value (e.g., (By.ID, 'element_id')).
+
+        Returns:
+                - The located WebElement.
+        """
         return element.find_element(*location)
 
     @staticmethod
-    def find_elements(element, location):
+    def find_elements(element: WebElement, location: Tuple[str, str]) -> List[WebElement, ...]:
+        """
+            Finds and returns multiple WebElements within the specified element's scope.
+
+            Args:
+                - element: The parent WebElement within which to search.
+                - location: A tuple representing the location strategy and value (e.g., (By.ID, 'element_id')).
+
+            Returns:
+                - A list of located WebElements.
+        """
         return element.find_elements(*location)
 
     @staticmethod
-    def extract_text(element):
-        return element.text
+    def extract_text(element: WebElement) -> str:
+        """
+            Extracts the text content of the specified WebElement.
 
+            Args:
+                - element: The WebElement from which to extract text.
+
+            Returns:
+                - The text content of the WebElement.
+        """
+        return element.text
